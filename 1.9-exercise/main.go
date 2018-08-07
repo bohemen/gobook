82a305d3372bf179a1f8845eaaaa2278ea7e7676
Björn Sjöberg <ibs@bohemen.org>
2018-08-07 20:41:05 +0200

    Update

--- 1.9-exercise/main.go
+++ 1.9-exercise/main.go
@@ -23,6 +23,7 @@ func main() {
 			fmt.Fprintf(os.Stderr, "%s: %v\n", app, err)
 			os.Exit(1)
 		}
+		fmt.Printf("HTTP status: %s\n", resp.Status)
 		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
 			fmt.Fprintf(os.Stderr, "%s: %v\n", app, err)
 			os.Exit(1)
